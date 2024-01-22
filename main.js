f936d3f6e4f7f236ba164e756891c08ba35a5f4d
Lennart Reinke <lennartreinke@yahoo.de>
2024-01-22 03:13:43 +0100

    Final touches before handing it in

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import openSimplexNoise from 'https://cdn.skypack.dev/open-simplex-noise';
 
+// Layout-Management
+let first = true;
+let started = false;
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({
@@ -22,31 +26,34 @@ const heightMap = textureLoader.load("./textures/abstract/Abstract_004_DISP.png"
 const roughness = textureLoader.load("./textures/abstract/Abstract_004_SPEC.jpg");
 const ambientOcclusion = textureLoader.load("./textures/abstract/Abstract_004_OCC.jpg");
 
+const loadingManager = new THREE.LoadingManager();
+
 // Blender Model
 let modelOffset = 4.9 * (-1);
+let model1;
+let model2;
 let models = [];
-const loader = new GLTFLoader().setPath('./models/');
-loader.load('subway-model-1-with-material-joined.glb', (glb1) => {
-	const model1 = glb1.scene;
-	
-
-	model1.position.setZ(0);
-
-	loader.load('subway-model-2-with-material-joined.glb', (glb2) => {
-		const model2 = glb2.scene;
-
-		for (let x = 1; x <= 20; x++) {
-			let newModel = Math.floor(Math.random() * 2) < 1 ? model1.clone() : model2.clone();
-			newModel.position.set(0, 0, modelOffset * x);
-			scene.add(newModel);
-	
-			models.push(newModel);
-		}
-
-		generateBackBox();
-
-	});
+const loader = new GLTFLoader(loadingManager).setPath('./models/');
+loader.load('subway-model-1-with-material-joined.glb', (glb) => {
+	model1 = glb.scene;
 });
+
+loader.load('subway-model-2-with-material-joined.glb', (glb) => {
+	model2 = glb.scene;
+});
+
+loadingManager.onLoad = function() {
+	for (let x = 1; x <= 20; x++) {
+		let newModel = Math.floor(Math.random() * 2) < 1 ? model1.clone() : model2.clone();
+		newModel.position.set(0, 0, modelOffset * x);
+		scene.add(newModel);
+
+		models.push(newModel);
+	}
+
+	generateBackBox();
+}
+
 
 // Geometry
 // Texture by Joao Paulo from https://3dtextures.me
@@ -60,7 +67,7 @@ const sphere = new THREE.Mesh(geometry,
         aoMap: ambientOcclusion
     }));
 
-sphere.position.set(0, 1.5, -25);
+sphere.position.set(0, 1.5, -30);
 scene.add( sphere );
 
 const count = geometry.attributes.position.count;
@@ -81,9 +88,11 @@ const initialDist = camera.position.z;
 function updateCamera() {
     camera.updateProjectionMatrix();
 
+	// Walking
 	camera.position.y += Math.sin(Date.now() * 0.008) * 0.003;
 
-    if (isZoomingIn && camera.position.z - initialDist < 1.25) {
+	// Dolly-Effect - when the camera moves the fox has to change accordingly
+    if (isZoomingIn && camera.position.z - initialDist < 1.5) {
 		camera.position.z += ((window.innerWidth / window.innerHeight) / (0.5 * Math.tan(1 * ((Math.PI * 2) / 360)) * camera.fov)) / 1000;
 		camera.fov -= zoomSpeed;
 	} else if (isZoomingOut && camera.position.z - initialDist > 0) {
@@ -94,24 +103,6 @@ function updateCamera() {
 	}
 
 }
-
-// Event listeners for keyboard input
-document.addEventListener('keydown', (event) => {
-    if (event.key === 'w') {
-        isZoomingIn = true;
-        isZoomingOut = false;
-    } else if (event.key === 's') {
-        isZoomingIn = false;
-        isZoomingOut = true;
-    }
-});
-
-document.addEventListener('keyup', (event) => {
-    if (event.key === 'w' || event.key === 's') {
-        isZoomingIn = false;
-        isZoomingOut = false;
-    }
-});
 
 // Audio
 const listener = new THREE.AudioListener();
@@ -127,35 +118,50 @@ const eerie = new THREE.Audio ( listener );
 
 const audioLoader = new THREE.AudioLoader();
 
-document.addEventListener('click', () => {
-	audioLoader.load( './sounds/footsteps-on-wood-floor-14735.mp3', function( buffer ) {
-		walking.setBuffer( buffer );
-		walking.setLoop( true );
-		walking.setVolume( 0.8 );
-		walking.play();
-	});
+// Event listeners for keyboard input for the dolly-zoom effect
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'w') {
+        isZoomingIn = true;
+        isZoomingOut = false;
+    } else if (event.key === 's') {
+        isZoomingIn = false;
+        isZoomingOut = true;
+    } else if (event.keyCode === 13 && first === false) {
+		document.querySelector('.loading-screen').style.display = 'none';
+		started = true;
 
-	audioLoader.load( './sounds/subway-passing-99262.mp3', function( buffer ) {
-		train.setBuffer( buffer );
-		train.setLoop( true );
-		train.setVolume( 0.5 );
-		train.play();
-	});
+		audioLoader.load( './sounds/footsteps-on-wood-floor-14735.mp3', function( buffer ) {
+			walking.setBuffer( buffer );
+			walking.setLoop( true );
+			walking.setVolume( 0.8 );
+			walking.play();
+		});
+	
+		audioLoader.load( './sounds/subway-passing-99262.mp3', function( buffer ) {
+			train.setBuffer( buffer );
+			train.setLoop( true );
+			train.setVolume( 0.5 );
+			train.play();
+		});
+	
+		audioLoader.load( './sounds/eerie-ambience-6836.mp3', function( buffer ) {
+			eerie.setBuffer( buffer );
+			eerie.setLoop( true );
+			eerie.setVolume( 0.25 );
+			eerie.play();
+		});
+	}
+});
 
-	audioLoader.load( './sounds/eerie-ambience-6836.mp3', function( buffer ) {
-		eerie.setBuffer( buffer );
-		eerie.setLoop( true );
-		eerie.setVolume( 0.25 );
-		eerie.play();
-	});
-})
+document.addEventListener('keyup', (event) => {
+    if (event.key === 'w' || event.key === 's') {
+        isZoomingIn = false;
+        isZoomingOut = false;
+    }
+});
 
-sphere.add(eerie);
 
-// Controll-Objects for the distortion of the object
-const clock = new THREE.Clock();
-let noise = openSimplexNoise.makeNoise4D(Date.now());
-
+// Generates a box at the "end" of the train to build a budget-backdoor
 function generateBackBox() {
 	const bg = new THREE.BoxGeometry( 10, 10, 10 ); 
 	const bm = new THREE.MeshBasicMaterial( {color: 0x2b2b2b} ); 
@@ -164,56 +170,78 @@ function generateBackBox() {
 	scene.add( cube );
 }
 
-// For FPS
+// Controll-Objects for the distortion of the object
+const clock = new THREE.Clock();
+let noise = openSimplexNoise.makeNoise4D(Date.now());
+
+// For FPS measurement
 let frames = 0;
 let prevTime = performance.now();
 
+// Limiting FPS
+let delta = 0;
+let interval = 1 / 90;
+
 function animate() {
 	requestAnimationFrame( animate );
+	
+	delta += clock.getDelta();
 
-	sphere.rotation.x += 0.003;
-	sphere.rotation.z += 0.003;
+	if (delta > interval) {
+		sphere.rotation.x += 0.003;
+		sphere.rotation.z += 0.003;
 
-	let elapsed = clock.getElapsedTime();
-    geometry.positionData.forEach((p, idx) => {
-        // Create noise for each point in the sphere
-        let setNoise = noise(p.x, p.y, p.z, elapsed * 1.05);
-        v3.copy(p).addScaledVector(p, setNoise);
-        // Update the positions
-        geometry.attributes.position.setXYZ(idx, v3.x, v3.y, v3.z);
-    });
+		let elapsed = clock.getElapsedTime();
 
-	geometry.computeVertexNormals();
-    geometry.attributes.position.needsUpdate = true;
+		for(let i = 0; i < geometry.positionData.length; i++) {
+			const p = geometry.positionData[i];
+			// Create noise for each point in the sphere
+			let setNoise = noise(p.x, p.y, p.z, elapsed * 1.05);
+			v3.copy(p).addScaledVector(p, setNoise);
+			// Update the positions
+			geometry.attributes.position.setXYZ(i, v3.x, v3.y, v3.z);
+		}
 
-	updateCamera();
+		geometry.computeVertexNormals();
+		geometry.attributes.position.needsUpdate = true;
 
-	if (models.length) models.forEach(model => {
-		if (Math.floor(model.position.z) > 2) { 
-			model.position.setZ(modelOffset * models.length + model.position.z)
-		} else {
-			model.position.z += 0.03;
-		};
-	});
+		updateCamera();
+
+		if (models.length) for(let i = 0; i < models.length; i++) {
+			const model = models[i];
+			if (Math.floor(model.position.z) > 2) { 
+				model.position.setZ(modelOffset * models.length + model.position.z)
+			} else {
+				model.position.z += 0.02;
+			};
+		}
 
 
-	// FPS counter
-	frames ++;
-    const time = performance.now();
-    
-    if ( time >= prevTime + 1000 ) {
-    
-    	console.log( Math.round( ( frames * 1000 ) / ( time - prevTime ) ) );
-      
-      frames = 0;
-      prevTime = time;
-      
-    }
+		// FPS counter
+		frames ++;
+		const time = performance.now();
 
-	renderer.render( scene, camera );
+		if ( time >= prevTime + 1000 ) {
 
-	console.log(renderer.info.render.calls);
+			if (first) {
+				first = false;
+				document.querySelector('.lds-dual-ring').style.display = 'none';
+				document.querySelector('.continue-container').style.opacity = '1';
+			}
+		
+			console.log( Math.round( ( frames * 1000 ) / ( time - prevTime ) ) );
+		
+			frames = 0;
+			prevTime = time;
+		
+		}
+
+		renderer.render( scene, camera );
+
+		delta = delta % interval;
+	}
+
+	// console.log(renderer.info.render.calls);
 }
-
 
 animate();
